7a0821b27d6568453e4bb53ed874077fb32850b6
Edgar018 <edgar_belandria010@hotmail.com>
2022-08-31 20:10:35 -0400

    delete express server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
-app.listen(3000, () => {
-  console.log("server on port 3000")
-
-});
-
 const { config } = require("dotenv");
 
 config();
