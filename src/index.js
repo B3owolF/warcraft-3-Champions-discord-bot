189202958f2f39ed128efc3a2554e50266a06de0
Edgar018 <edgar_belandria010@hotmail.com>
2022-09-02 21:09:05 -0400

    feat: add express server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(3000, () => {
+  console.log("server on port 3000")
+
+});
+
 const { config } = require("dotenv");
 
 config();
